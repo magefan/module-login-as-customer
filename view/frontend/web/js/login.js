406bcdf57fe37be74740cf4ba657b273fa6d1280
Ihor Vansach <ihor@magefan.com>
2020-05-08 08:47:59 +0300

    Fix for Magento  2.3.5-p1

--- view/frontend/web/js/login.js
+++ view/frontend/web/js/login.js
@@ -4,13 +4,16 @@
  */
 
 define([
-    'Magento_Customer/js/customer-data'
-], function (customerData) {
+    'jquery',
+    'Magento_Customer/js/customer-data',
+    'Magento_Customer/js/section-config'
+], function ($, customerData, sectionConfig) {
 
     'use strict';
 
     return function (config) {
-        customerData.reload('customer').done(function () {
+        $('body').trigger('processStart');
+        customerData.reload(sectionConfig.getSectionNames()).done(function () {
             window.location.href = config.redirectUrl;
         });
     };
